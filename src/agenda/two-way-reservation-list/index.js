f1ce40cbab355380c5b72f558e3e268c270729e8
frizzonelli <fabrizio.rizzonelli@gmail.com>
2018-08-29 10:03:43 +0200

    fixed scroll glitch

--- src/agenda/two-way-reservation-list/index.js
+++ src/agenda/two-way-reservation-list/index.js
@@ -170,20 +170,20 @@ class ReactComp extends Component {
       return { reservations: [], scrollPosition: 0 };
     }
     let reservations = [];
-    if (this.state.reservations && this.state.reservations.length) {
-      const iterator = this.state.reservations[0].day.clone();
-      while (iterator.getTime() < props.selectedDay.getTime()) {
-        const res = this.getReservationsForDay(iterator, props);
-        if (!res) {
-          reservations = [];
-          break;
-        } else {
-          reservations = reservations.concat(res);
-        }
-        iterator.addDays(1);
-      }
-    }
-    const scrollPosition = reservations.length;
+    // if (this.state.reservations && this.state.reservations.length) {
+    //   const iterator = this.state.reservations[0].day.clone();
+    //   while (iterator.getTime() < props.selectedDay.getTime()) {
+    //     const res = this.getReservationsForDay(iterator, props);
+    //     if (!res) {
+    //       reservations = [];
+    //       break;
+    //     } else {
+    //       reservations = reservations.concat(res);
+    //     }
+    //     iterator.addDays(1);
+    //   }
+    // }
+    // const scrollPosition = reservations.length;
     // const iterator = props.selectedDay.clone();
     // for (let i = 0; i < 31; i++) {
     //   const res = this.getReservationsForDay(iterator, props);
@@ -192,8 +192,16 @@ class ReactComp extends Component {
     //   }
     //   iterator.addDays(1);
     // }
+
+    let scrollPosition = 0;
+
     for (let i = 0; i < Object.keys(props.reservations).length; i++) {
       const day = XDate(new Date(Object.keys(props.reservations)[i]));
+
+      if (day.getTime() < props.selectedDay.getTime()) {
+        scrollPosition++;
+      }
+
       const res = this.getReservationsForDay(day, props);
       if (!res) {
         reservations = [];
